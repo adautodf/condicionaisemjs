01c10ff4a5b31ec1300b7e8c1f111f4fde9a21d5
unknown <adautodortafilho@gmail.com>
2022-01-26 17:15:40 -0300

    nova implementação aplicada (embarque)

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -8,29 +8,35 @@ const listaDeDestinos = displayedColumns = [
     'Curitiba',
 ];
 
-const idadeComprador = 15;
-const estaAcompanhada = false;
+const idadeComprador = 17;
+const estaAcompanhada = true;
+var temPassagemComprada;
 
 console.log("Destinos disponíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(0, 1); //removendo item
-} else if (estaAcompanhada) {
-    console.log("Comprador está acompanhado");
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    temPassagemComprada = true;
+    console.log("Seu destino foi selecionado com sucesso");
     listaDeDestinos.splice(0, 1); //removendo item
 } else {
-    console.log("Não será possível efetuar a compra devido ao comprador ser Menor de idade ou está Desacompanhado");
+    console.log("Não será possível efetuar a compra devido ao comprador ser Menor de idade e não estar acompanhado");
+    temPassagemComprada = false;
 }
 
 console.log(listaDeDestinos);
 
 // Informações sobre o Comprador
 console.log(idadeComprador >= 18 ? 'Comprador é maior de idade' : 'comprador menor de idade' )
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador == 18);
+//console.log(idadeComprador > 18);
+//console.log(idadeComprador < 18);
+//console.log(idadeComprador >= 18);
+//console.log(idadeComprador <= 18);
+//console.log(idadeComprador == 18);
 
+console.log("Embarque:  \n")
+if(temPassagemComprada && (idadeComprador >=18 || estaAcompanhada)) { 
+    console.log("Boa viagem!");
+}else{
+    console.log("Você não pode embarcar");
+}
